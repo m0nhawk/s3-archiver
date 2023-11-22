5b618e2058cfeb616d34f522cd327aaff4a18cd4
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2023-11-22 00:55:25 -0600

    update nix

--- flake.nix
+++ flake.nix
@@ -1,45 +1,21 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      with nixpkgs.legacyPackages.${system};
-      let
-        t = lib.trivial;
-        hl = haskell.lib;
-
-        name = "project-name";
-
-        project = devTools: # [1]
-          let addBuildTools = (t.flip hl.addBuildTools) devTools;
-          in haskellPackages.developPackage {
-            root = lib.sourceFilesBySuffices ./. [ ".cabal" ".hs" ];
-            name = name;
-            returnShellEnv = !(devTools == [ ]); # [2]
-
-            modifier = (t.flip t.pipe) [
-              addBuildTools
-              hl.dontHaddock
-              hl.enableStaticLibraries
-              hl.justStaticExecutables
-              hl.disableLibraryProfiling
-              hl.disableExecutableProfiling
-            ];
-          };
-
-      in {
-        packages.pkg = project [ ]; # [3]
-
-        defaultPackage = self.packages.${system}.pkg;
-
-        devShell = project (with haskellPackages; [ # [4]
-          cabal-fmt
-          cabal-install
-          haskell-language-server
-          hlint
-        ]);
-      });
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      overlay = final: prev: {
+        s3-archiver = final.callCabal2nix "app" ./. { };
+      };
+      myHaskellPackages = pkgs.haskellPackages.extend overlay;
+    in
+    {
+      packages.${system}.default = myHaskellPackages.s3-archiver;
+      apps.${system}.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/s3-archiver-exe";
+      };
+    };
 }
